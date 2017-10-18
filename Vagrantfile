f62faec68f56e24f67e192fc36778d6fe55bc329
John Rofrano <johnnyroy@johnrofrano.com>
2017-10-18 11:35:48 -0400

    updated for xenial64 and latest model

--- Vagrantfile
+++ Vagrantfile
@@ -9,18 +9,16 @@ Vagrant.configure(2) do |config|
   # The most common configuration options are documented and commented below.
   # For a complete reference, please see the online documentation at
   # https://docs.vagrantup.com.
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = "ubuntu/xenial64"
   config.vm.network "forwarded_port", guest: 5000, host: 5000, host_ip: "127.0.0.1"
   config.vm.network "private_network", ip: "192.168.33.10"
 
   # Windows users need to change the permissions explicitly so that Windows doesn't
   # set the execute bit on all of your files which messes with GitHub users on Mac and Linux
-  config.vm.synced_folder "./", "/vagrant", owner: "vagrant", mount_options: ["dmode=755,fmode=644"]
+  config.vm.synced_folder "./", "/vagrant", owner: "ubuntu", mount_options: ["dmode=755,fmode=644"]
 
   # Example for VirtualBox:
   config.vm.provider "virtualbox" do |vb|
-    # Display the VirtualBox GUI when booting the machine
-    #vb.gui = true
     # Customize the amount of memory on the VM:
     vb.memory = "512"
     vb.cpus = 1
@@ -31,16 +29,25 @@ Vagrant.configure(2) do |config|
     config.vm.provision "file", source: "~/.gitconfig", destination: "~/.gitconfig"
   end
 
+  # Copy your ssh keys for github so that your git credentials work
+  if File.exists?(File.expand_path("~/.ssh/id_rsa"))
+    config.vm.provision "file", source: "~/.ssh/id_rsa", destination: "~/.ssh/id_rsa"
+  end
+
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
   config.vm.provision "shell", inline: <<-SHELL
-    sudo apt-get update
-    sudo apt-get install -y git python-pip python-dev build-essential zip
-    sudo apt-get -y autoremove
-    # Install Foreman for Profile support
-    sudo gem install foreman --no-rdoc --no-ri
+    apt-get update
+    apt-get install -y git python-pip python-dev
+    apt-get -y autoremove
+    pip install --upgrade pip
+    # Make vi look nice
+    sudo -H -u ubuntu echo "colorscheme desert" > ~/.vimrc
     # Install app dependencies
+    echo "\n******************************"
+    echo " Installing App Dependencies"
+    echo "******************************\n"
     cd /vagrant
     sudo pip install -r requirements.txt
   SHELL
@@ -51,7 +58,7 @@ Vagrant.configure(2) do |config|
   config.vm.provision "shell", inline: <<-SHELL
     # Prepare Redis data share
     sudo mkdir -p /var/lib/redis/data
-    sudo chown vagrant:vagrant /var/lib/redis/data
+    sudo chown ubuntu:ubuntu /var/lib/redis/data
   SHELL
 
   # Add Redis docker container
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-# Copyright 2016 John J. Rofrano. All Rights Reserved.
+# Copyright 2016, 2017 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,175 +12,269 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Pet API Controller
+
+This modules provides a REST API for the Pet Model
+
+Paths:
+------
+GET /pets - Lists all of the Pets
+GET /pets/{id} - Retrieves a single Pet with the specified id
+POST /pets - Creates a new Pet
+PUT /pets/{id} - Updates a single Pet with the specified id
+DELETE /pets/{id} - Deletes a single Pet with the specified id
+POST /pets/{id}/purchase - Action to purchase a Pet
+"""
+
 import os
-from redis import Redis
-from redis.exceptions import ConnectionError
-from flask import Flask, Response, jsonify, request, json, url_for, make_response
-from flask_api import status    # HTTP Status Codes
-from werkzeug.exceptions import NotFound
+import sys
+import logging
+from flask import Flask, jsonify, request, url_for, make_response, abort
 from models import Pet, DataValidationError
 
 # Create Flask application
 app = Flask(__name__)
+app.config['LOGGING_LEVEL'] = logging.INFO
 
 # Pull options from environment
-debug = (os.getenv('DEBUG', 'False') == 'True')
-port = os.getenv('PORT', '5000')
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+PORT = os.getenv('PORT', '5000')
+
+# Status Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+HTTP_400_BAD_REQUEST = 400
+HTTP_404_NOT_FOUND = 404
+HTTP_409_CONFLICT = 409
+HTTP_415_UNSUPPORTED_MEDIA_TYPE = 415
 
 ######################################################################
-# ERROR Handling
+# Error Handlers
 ######################################################################
-
 @app.errorhandler(DataValidationError)
-def request_validation_error(e):
-    return bad_request(e)
-
-@app.errorhandler(404)
-def not_found(e):
-    return make_response(jsonify(status=404, error='Not Found', message=e.description), status.HTTP_404_NOT_FOUND)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
 
 @app.errorhandler(400)
-def bad_request(e):
-    return make_response(jsonify(status=400, error='Bad Request', message=e.message), status.HTTP_400_BAD_REQUEST)
+def bad_request(error):
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=400, error='Bad Request', message=message), 400
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Handles resources not found with 404_NOT_FOUND """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=404, error='Not Found', message=message), 404
 
 @app.errorhandler(405)
-def method_not_allowed(e):
-    return make_response(jsonify(status=405, error='Method not Allowed', message='Your request method is not supported. Check your HTTP method and try again.'), status.HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_supported(error):
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=405, error='Method not Allowed', message=message), 405
+
+@app.errorhandler(415)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=415, error='Unsupported media type', message=message), 415
 
 @app.errorhandler(500)
-def internal_error(e):
-    return make_response(jsonify(status=500, error='Internal Server Error', message='Huston... we have a problem.'), status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    message = error.message or str(error)
+    app.logger.info(message)
+    return jsonify(status=500, error='Internal Server Error', message=message), 500
+
 
 ######################################################################
 # GET INDEX
 ######################################################################
 @app.route('/')
 def index():
-    data = '{name: <string>, category: <string>}'
-    url = request.base_url + 'pets' # url_for('list_pets')
-    return jsonify(name='Pet Demo REST API Service', version='1.0', url=url, data=data), status.HTTP_200_OK
+    """ Send back the home page """
+    return app.send_static_file('index.html')
 
 ######################################################################
 # LIST ALL PETS
 ######################################################################
 @app.route('/pets', methods=['GET'])
 def list_pets():
+    """ Returns all of the Pets """
     pets = []
     category = request.args.get('category')
+    name = request.args.get('name')
+    available = request.args.get('available')
     if category:
         pets = Pet.find_by_category(category)
+    elif name:
+        pets = Pet.find_by_name(name)
+    elif available:
+        pets = Pet.find_by_availability(available)
     else:
         pets = Pet.all()
 
     results = [pet.serialize() for pet in pets]
-    app.logger.info("results={}".format(results))
-    j = jsonify(results)
-    app.logger.info("json={}".format(j))
-    return make_response(jsonify(results), status.HTTP_200_OK)
+    return make_response(jsonify(results), HTTP_200_OK)
 
 ######################################################################
 # RETRIEVE A PET
 ######################################################################
-@app.route('/pets/<int:id>', methods=['GET'])
-def get_pets(id):
-    pet = Pet.find_or_404(id)
-    return make_response(jsonify(pet.serialize()), status.HTTP_200_OK)
+@app.route('/pets/<int:pet_id>', methods=['GET'])
+def get_pets(pet_id):
+    """
+    Retrieve a single Pet
+
+    This endpoint will return a Pet based on it's id
+    """
+    pet = Pet.find(pet_id)
+    if not pet:
+        abort(HTTP_404_NOT_FOUND, "Pet with id '{}' was not found.".format(pet_id))
+    return make_response(jsonify(pet.serialize()), HTTP_200_OK)
 
 ######################################################################
 # ADD A NEW PET
 ######################################################################
 @app.route('/pets', methods=['POST'])
 def create_pets():
+    """
+    Creates a Pet
+
+    This endpoint will create a Pet based the data in the body that is posted
+    or data that is sent via an html form post.
+    """
+    check_content_type('application/json')
     data = {}
     # Check for form submission data
     if request.headers.get('Content-Type') == 'application/x-www-form-urlencoded':
-        data = {'name': request.form['name'], 'category': request.form['category']}
+        app.logger.info('Processing FORM data')
+        data = {
+            'name': request.form['name'],
+            'category': request.form['category'],
+            'available': request.form['available'].lower() in ['true', '1', 't']
+        }
     else:
+        app.logger.info('Processing JSON data')
         data = request.get_json()
     pet = Pet()
     pet.deserialize(data)
     pet.save()
     message = pet.serialize()
-    return make_response(jsonify(message), status.HTTP_201_CREATED, {'Location': pet.self_url() })
+    return make_response(jsonify(message), HTTP_201_CREATED,
+                         {'Location': url_for('get_pets', pet_id=pet.id, _external=True)})
 
 ######################################################################
 # UPDATE AN EXISTING PET
 ######################################################################
-@app.route('/pets/<int:id>', methods=['PUT'])
-def update_pets(id):
-    pet = Pet.find_or_404(id)
+@app.route('/pets/<int:pet_id>', methods=['PUT'])
+def update_pets(pet_id):
+    """
+    Update a Pet
+
+    This endpoint will update a Pet based the body that is posted
+    """
+    check_content_type('application/json')
+    pet = Pet.find(pet_id)
+    if not pet:
+        abort(HTTP_404_NOT_FOUND, "Pet with id '{}' was not found.".format(pet_id))
     pet.deserialize(request.get_json())
+    pet.id = pet_id
     pet.save()
-    return make_response(jsonify(pet.serialize()), status.HTTP_200_OK)
+    return make_response(jsonify(pet.serialize()), HTTP_200_OK)
+
 
 ######################################################################
 # DELETE A PET
 ######################################################################
-@app.route('/pets/<int:id>', methods=['DELETE'])
-def delete_pets(id):
-    pet = Pet.find(id)
+@app.route('/pets/<int:pet_id>', methods=['DELETE'])
+def delete_pets(pet_id):
+    """
+    Delete a Pet
+
+    This endpoint will delete a Pet based the id specified in the path
+    """
+    pet = Pet.find(pet_id)
     if pet:
         pet.delete()
-    return make_response('', status.HTTP_204_NO_CONTENT)
+    return make_response('', HTTP_204_NO_CONTENT)
+
+######################################################################
+# PURCHASE A PET
+######################################################################
+@app.route('/pets/<int:pet_id>/purchase', methods=['PUT'])
+def purchase_pets(pet_id):
+    """ Purchase a Pet """
+    pet = Pet.find(pet_id)
+    if not pet:
+        abort(HTTP_404_NOT_FOUND, "Pet with id '{}' was not found.".format(pet_id))
+    if not pet.available:
+        abort(HTTP_400_BAD_REQUEST, "Pet with id '{}' is not available.".format(pet_id))
+    pet.available = False
+    pet.save()
+    return make_response(jsonify(pet.serialize()), HTTP_200_OK)
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
+
+@app.before_first_request
+def init_db(redis=None):
+    """ Initlaize the model """
+    Pet.init_db(redis)
+
 # load sample data
-def data_load(data):
-    Pet().deserialize(data).save()
+def data_load(payload):
+    """ Loads a Pet into the database """
+    pet = Pet(0, payload['name'], payload['category'])
+    pet.save()
 
-# empty the database
 def data_reset():
-    redis.flushall()
+    """ Removes all Pets from the database """
+    Pet.remove_all()
 
-######################################################################
-# Connect to Redis and catch connection exceptions
-######################################################################
-def connect_to_redis(hostname, port, password):
-    redis = Redis(host=hostname, port=port, password=password)
-    try:
-        redis.ping()
-    except ConnectionError:
-        redis = None
-    return redis
+def check_content_type(content_type):
+    """ Checks that the media type is correct """
+    if request.headers['Content-Type'] == content_type:
+        return
+    app.logger.error('Invalid Content-Type: %s', request.headers['Content-Type'])
+    abort(HTTP_415_UNSUPPORTED_MEDIA_TYPE, 'Content-Type must be {}'.format(content_type))
 
-
-######################################################################
-# INITIALIZE Redis
-# This method will work in the following conditions:
-#   1) In Bluemix with Redsi bound through VCAP_SERVICES
-#   2) With Redis running on the local server as with Travis CI
-#   3) With Redis --link ed in a Docker container called 'redis'
-######################################################################
-def inititalize_redis():
-    global redis
-    redis = None
-    # Get the crdentials from the Bluemix environment
-    if 'VCAP_SERVICES' in os.environ:
-        app.logger.info("Using VCAP_SERVICES...")
-        VCAP_SERVICES = os.environ['VCAP_SERVICES']
-        services = json.loads(VCAP_SERVICES)
-        creds = services['rediscloud'][0]['credentials']
-        app.logger.info("Conecting to Redis on host %s port %s" % (creds['hostname'], creds['port']))
-        redis = connect_to_redis(creds['hostname'], creds['port'], creds['password'])
-    else:
-        app.logger.info("VCAP_SERVICES not found, checking localhost for Redis")
-        redis = connect_to_redis('127.0.0.1', 6379, None)
-        if not redis:
-            app.logger.info("No Redis on localhost, using: redis")
-            redis = connect_to_redis('redis', 6379, None)
-    if not redis:
-        # if you end up here, redis instance is down.
-        app.logger.error('*** FATAL ERROR: Could not connect to the Redis Service')
-    # Have the Pet model use Redis
-    Pet.use_db(redis)
+# @app.before_first_request
+def initialize_logging(log_level=logging.INFO):
+    """ Initialized the default logging to STDOUT """
+    if not app.debug:
+        print 'Setting up logging...'
+        # Set up default logging for submodules to use STDOUT
+        # datefmt='%m/%d/%Y %I:%M:%S %p'
+        fmt = '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+        logging.basicConfig(stream=sys.stdout, level=log_level, format=fmt)
+        # Make a new log handler that uses STDOUT
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(logging.Formatter(fmt))
+        handler.setLevel(log_level)
+        # Remove the Flask default handlers and use our own
+        handler_list = list(app.logger.handlers)
+        for log_handler in handler_list:
+            app.logger.removeHandler(log_handler)
+        app.logger.addHandler(handler)
+        app.logger.setLevel(log_level)
+        app.logger.info('Logging handler established')
 
 
 ######################################################################
 #   M A I N
 ######################################################################
 if __name__ == "__main__":
-    print "Pet Service Starting..."
-    inititalize_redis()
-    app.run(host='0.0.0.0', port=int(port), debug=debug)
+    print "************************************************************"
+    print "        P E T   R E S T   A P I   S E R V I C E "
+    print "************************************************************"
+    initialize_logging(app.config['LOGGING_LEVEL'])
+    app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
