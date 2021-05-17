89f8de84e8fdbf3295174f6f0db6c6f4b0550aa2
John Rofrano <johnnyroy@johnrofrano.com>
2021-05-17 11:55:34 -0400

    Added REDIS_HOST env support

--- service/models.py
+++ service/models.py
@@ -29,108 +29,112 @@ import logging
 from redis import StrictRedis
 from redis.exceptions import ConnectionError
 
+
 class DataValidationError(Exception):
-    """ Custom Exception with data validation fails """
+    """Custom Exception with data validation fails"""
+
     pass
 
+
 class Pet(object):
-    """ Pet interface to database """
+    """Pet interface to database"""
 
     logger = logging.getLogger(__name__)
     redis = None
 
     def __init__(self, id=0, name=None, category=None, available=True):
-        """ Constructor """
+        """Constructor"""
         self.id = int(id)
         self.name = name
         self.category = category
         self.available = available
 
     def save(self):
-        """ Saves a Pet in the database """
-        if self.name is None:   # name is the only required field
-            raise DataValidationError('name attribute is not set')
+        """Saves a Pet in the database"""
+        if self.name is None:  # name is the only required field
+            raise DataValidationError("name attribute is not set")
         if self.id == 0:
             self.id = Pet.__next_index()
         Pet.redis.set(self.id, json.dumps(self.serialize()))
 
     def delete(self):
-        """ Deletes a Pet from the database """
+        """Deletes a Pet from the database"""
         Pet.redis.delete(self.id)
 
     def serialize(self):
-        """ serializes a Pet into a dictionary """
+        """serializes a Pet into a dictionary"""
         return {
             "id": self.id,
             "name": self.name,
             "category": self.category,
-            "available": self.available
+            "available": self.available,
         }
 
     def deserialize(self, data):
-        """ deserializes a Pet my marshalling the data """
+        """deserializes a Pet my marshalling the data"""
         try:
-            self.name = data['name']
-            self.category = data['category']
-            self.available = data['available']
+            self.name = data["name"]
+            self.category = data["category"]
+            self.available = data["available"]
         except KeyError as error:
-            raise DataValidationError('Invalid pet: missing ' + error.args[0])
+            raise DataValidationError("Invalid pet: missing " + error.args[0])
         except TypeError as error:
-            raise DataValidationError('Invalid pet: body of request contained bad or no data')
+            raise DataValidationError(
+                "Invalid pet: body of request contained bad or no data"
+            )
         return self
 
-
-######################################################################
-#  S T A T I C   D A T A B S E   M E T H O D S
-######################################################################
+    ######################################################################
+    #  S T A T I C   D A T A B S E   M E T H O D S
+    ######################################################################
 
     @classmethod
     def __next_index(cls):
-        """ Increments the index and returns it """
-        return cls.redis.incr('index')
+        """Increments the index and returns it"""
+        return cls.redis.incr("index")
 
     @classmethod
     def remove_all(cls):
-        """ Removes all Pets from the database """
+        """Removes all Pets from the database"""
         cls.redis.flushall()
 
     @classmethod
     def all(cls):
-        """ Query that returns all Pets """
+        """Query that returns all Pets"""
         # results = [cls.from_dict(redis.hgetall(key)) for key in redis.keys() if key != 'index']
         results = []
         for key in cls.redis.keys():
-            if key != 'index':  # filer out our id index
+            if key != "index":  # filer out our id index
                 data = json.loads(cls.redis.get(key))
-                pet = Pet(data['id']).deserialize(data)
+                pet = Pet(data["id"]).deserialize(data)
                 results.append(pet)
         return results
 
-######################################################################
-#  F I N D E R   M E T H O D S
-######################################################################
+    ######################################################################
+    #  F I N D E R   M E T H O D S
+    ######################################################################
 
     @classmethod
     def find(cls, pet_id):
-        """ Query that finds Pets by their id """
+        """Query that finds Pets by their id"""
         if cls.redis.exists(pet_id):
             data = json.loads(cls.redis.get(pet_id))
-            pet = Pet(data['id']).deserialize(data)
+            pet = Pet(data["id"]).deserialize(data)
             return pet
         return None
 
     @classmethod
     def __find_by(cls, attribute, value):
-        """ Generic Query that finds a key with a specific value """
+        """Generic Query that finds a key with a specific value"""
         # return [pet for pet in Pet.__data if pet.category == category]
-        cls.logger.info('Processing %s query for %s', attribute, value)
+        cls.logger.info("Processing %s query for %s", attribute, value)
         if isinstance(value, str):
-            search_criteria = value.lower() # make case insensitive
+            search_criteria = value.lower()  # make case insensitive
         else:
             search_criteria = value
         results = []
         for key in Pet.redis.keys():
-            if key != 'index':  # filer out our id index
+            if key != "index":  # filer out our id index
                 data = json.loads(Pet.redis.get(key))
                 # perform case insensitive search on strings
                 if isinstance(data[attribute], str):
@@ -138,37 +142,39 @@ class Pet(object):
                 else:
                     test_value = data[attribute]
                 if test_value == search_criteria:
-                    results.append(Pet(data['id']).deserialize(data))
+                    results.append(Pet(data["id"]).deserialize(data))
         return results
 
     @classmethod
     def find_by_name(cls, name):
-        """ Query that finds Pets by their name """
-        return cls.__find_by('name', name)
+        """Query that finds Pets by their name"""
+        return cls.__find_by("name", name)
 
     @classmethod
     def find_by_category(cls, category):
-        """ Query that finds Pets by their category """
-        return cls.__find_by('category', category)
+        """Query that finds Pets by their category"""
+        return cls.__find_by("category", category)
 
     @classmethod
     def find_by_availability(cls, available=True):
-        """ Query that finds Pets by their availability """
-        return cls.__find_by('available', available)
+        """Query that finds Pets by their availability"""
+        return cls.__find_by("available", available)
 
-######################################################################
-#  R E D I S   D A T A B A S E   C O N N E C T I O N   M E T H O D S
-######################################################################
+    ######################################################################
+    #  R E D I S   D A T A B A S E   C O N N E C T I O N   M E T H O D S
+    ######################################################################
 
     @classmethod
     def connect_to_redis(cls, hostname, port, password):
-        """ Connects to Redis and tests the connection """
+        """Connects to Redis and tests the connection"""
         cls.logger.info("Testing Connection to: %s:%s", hostname, port)
-        cls.redis = StrictRedis(host=hostname,
-                                port=port,
-                                password=password,
-                                charset="utf-8",
-                                decode_responses=True)
+        cls.redis = StrictRedis(
+            host=hostname,
+            port=port,
+            password=password,
+            charset="utf-8",
+            decode_responses=True,
+        )
 
         try:
             cls.redis.ping()
@@ -202,24 +208,26 @@ class Pet(object):
             except ConnectionError:
                 cls.logger.error("Client Connection Error!")
                 cls.redis = None
-                raise ConnectionError('Could not connect to the Redis Service')
+                raise ConnectionError("Could not connect to the Redis Service")
             return
         # Get the credentials from the Bluemix environment
-        if 'VCAP_SERVICES' in os.environ:
+        if "VCAP_SERVICES" in os.environ:
             cls.logger.info("Using VCAP_SERVICES...")
-            vcap_services = os.environ['VCAP_SERVICES']
+            vcap_services = os.environ["VCAP_SERVICES"]
             services = json.loads(vcap_services)
-            creds = services['rediscloud'][0]['credentials']
-            cls.logger.info("Conecting to Redis on host %s port %s",
-                            creds['hostname'], creds['port'])
-            cls.connect_to_redis(creds['hostname'], creds['port'], creds['password'])
+            creds = services["rediscloud"][0]["credentials"]
+            cls.logger.info(
+                "Conecting to Redis on host %s port %s",
+                creds["hostname"],
+                creds["port"],
+            )
+            cls.connect_to_redis(creds["hostname"], creds["port"], creds["password"])
         else:
-            cls.logger.info("VCAP_SERVICES not found, checking localhost for Redis")
-            cls.connect_to_redis('127.0.0.1', 6379, None)
-            if not Pet.redis:
-                cls.logger.info("No Redis on localhost, looking for redis host")
-                cls.connect_to_redis('redis', 6379, None)
+            cls.logger.info("VCAP_SERVICES not found, checking REDIS_HOST for Redis")
+            REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
+            REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+            cls.connect_to_redis(REDIS_HOST, REDIS_PORT, None)
         if not Pet.redis:
             # if you end up here, redis instance is down.
-            cls.logger.fatal('*** FATAL ERROR: Could not connect to the Redis Service')
-            raise ConnectionError('Could not connect to the Redis Service')
+            cls.logger.fatal("*** FATAL ERROR: Could not connect to the Redis Service")
+            raise ConnectionError("Could not connect to the Redis Service")
--- tests/test_pets.py
+++ tests/test_pets.py
@@ -26,14 +26,12 @@ from unittest.mock import patch
 from redis import Redis, ConnectionError
 from service.models import Pet, DataValidationError
 
+REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+
 VCAP_SERVICES = {
-    'rediscloud': [
-        {'credentials': {
-            'password': '',
-            'hostname': '127.0.0.1',
-            'port': '6379'
-            }
-        }
+    "rediscloud": [
+        {"credentials": {"password": "", "hostname": REDIS_HOST, "port": REDIS_PORT}}
     ]
 }
 
@@ -41,10 +39,10 @@ VCAP_SERVICES = {
 #  T E S T   C A S E S
 ######################################################################
 class TestPets(unittest.TestCase):
-    """ Test Cases for Pet Model """
+    """Test Cases for Pet Model"""
 
     def setUp(self):
-        """ Initialize the Redis database """
+        """Initialize the Redis database"""
         Pet.init_db()
         Pet.remove_all()
         # Pet.init_db(Redis(host='127.0.0.1', port=6379))
@@ -52,7 +50,7 @@ class TestPets(unittest.TestCase):
         # Pet(0, 'kitty', 'cat').save()
 
     def test_create_a_pet(self):
-        """ Create a pet and assert that it exists """
+        """Create a pet and assert that it exists"""
         pet = Pet(0, "fido", "dog", False)
         self.assertNotEqual(pet, None)
         self.assertEqual(pet.id, 0)
@@ -61,11 +59,11 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pet.available, False)
 
     def test_add_a_pet(self):
-        """ Create a pet and add it to the database """
+        """Create a pet and add it to the database"""
         pets = Pet.all()
         self.assertEqual(pets, [])
         pet = Pet(0, "fido", "dog", True)
-        self.assertTrue(pet != None)
+        self.assertIsNotNone(pet)
         self.assertEqual(pet.id, 0)
         pet.save()
         # Asert that it was assigned an id and shows up in the database
@@ -78,7 +76,7 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pets[0].available, True)
 
     def test_update_a_pet(self):
-        """ Update a Pet """
+        """Update a Pet"""
         pet = Pet(0, "fido", "dog", True)
         pet.save()
         self.assertEqual(pet.id, 1)
@@ -94,7 +92,7 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pets[0].name, "fido")
 
     def test_delete_a_pet(self):
-        """ Delete a Pet """
+        """Delete a Pet"""
         pet = Pet(0, "fido", "dog")
         pet.save()
         self.assertEqual(len(Pet.all()), 1)
@@ -103,21 +101,21 @@ class TestPets(unittest.TestCase):
         self.assertEqual(len(Pet.all()), 0)
 
     def test_serialize_a_pet(self):
-        """ Serialize a Pet """
+        """Serialize a Pet"""
         pet = Pet(0, "fido", "dog")
         data = pet.serialize()
         self.assertNotEqual(data, None)
-        self.assertIn('id', data)
-        self.assertEqual(data['id'], 0)
-        self.assertIn('name', data)
-        self.assertEqual(data['name'], "fido")
-        self.assertIn('category', data)
-        self.assertEqual(data['category'], "dog")
+        self.assertIn("id", data)
+        self.assertEqual(data["id"], 0)
+        self.assertIn("name", data)
+        self.assertEqual(data["name"], "fido")
+        self.assertIn("category", data)
+        self.assertEqual(data["category"], "dog")
 
     def test_deserialize_a_pet(self):
-        """ Deserialize a Pet """
-        data = {"id":1, "name": "kitty", "category": "cat", "available": True}
-        pet = Pet(data['id'])
+        """Deserialize a Pet"""
+        data = {"id": 1, "name": "kitty", "category": "cat", "available": True}
+        pet = Pet(data["id"])
         pet.deserialize(data)
         self.assertNotEqual(pet, None)
         self.assertEqual(pet.id, 1)
@@ -125,28 +123,28 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pet.category, "cat")
 
     def test_deserialize_with_no_name(self):
-        """ Deserialize a Pet that has no name """
-        data = {"id":0, "category": "cat"}
+        """Deserialize a Pet that has no name"""
+        data = {"id": 0, "category": "cat"}
         pet = Pet(0)
         self.assertRaises(DataValidationError, pet.deserialize, data)
 
     def test_deserialize_with_no_data(self):
-        """ Deserialize a Pet that has no data """
+        """Deserialize a Pet that has no data"""
         pet = Pet(0)
         self.assertRaises(DataValidationError, pet.deserialize, None)
 
     def test_deserialize_with_bad_data(self):
-        """ Deserialize a Pet that has bad data """
+        """Deserialize a Pet that has bad data"""
         pet = Pet(0)
         self.assertRaises(DataValidationError, pet.deserialize, "string data")
 
     def test_save_a_pet_with_no_name(self):
-        """ Save a Pet with no name """
+        """Save a Pet with no name"""
         pet = Pet(0, None, "cat")
         self.assertRaises(DataValidationError, pet.save)
 
     def test_find_pet(self):
-        """ Find a Pet by id """
+        """Find a Pet by id"""
         Pet(0, "fido", "dog").save()
         Pet(0, "kitty", "cat").save()
         pet = Pet.find(2)
@@ -155,18 +153,18 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pet.name, "kitty")
 
     def test_find_with_no_pets(self):
-        """ Find a Pet with empty database """
+        """Find a Pet with empty database"""
         pet = Pet.find(1)
         self.assertIs(pet, None)
 
     def test_pet_not_found(self):
-        """ Find a Pet that doesnt exist """
+        """Find a Pet that doesnt exist"""
         Pet(0, "fido", "dog").save()
         pet = Pet.find(2)
         self.assertIs(pet, None)
 
     def test_find_by_name(self):
-        """ Find a Pet by Name """
+        """Find a Pet by Name"""
         Pet(0, "fido", "dog").save()
         Pet(0, "kitty", "cat").save()
         pets = Pet.find_by_name("fido")
@@ -175,7 +173,7 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pets[0].name, "fido")
 
     def test_find_by_category(self):
-        """ Find a Pet by Category """
+        """Find a Pet by Category"""
         Pet(0, "fido", "dog").save()
         Pet(0, "kitty", "cat").save()
         pets = Pet.find_by_category("cat")
@@ -184,7 +182,7 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pets[0].name, "kitty")
 
     def test_find_by_availability(self):
-        """ Find a Pet by Availability """
+        """Find a Pet by Availability"""
         Pet(0, "fido", "dog", False).save()
         Pet(0, "kitty", "cat", True).save()
         pets = Pet.find_by_availability(True)
@@ -192,7 +190,7 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pets[0].name, "kitty")
 
     def test_for_case_insensitive(self):
-        """ Test for Case Insensitive Search """
+        """Test for Case Insensitive Search"""
         Pet(0, "Fido", "DOG").save()
         Pet(0, "Kitty", "CAT").save()
         pets = Pet.find_by_name("fido")
@@ -203,24 +201,26 @@ class TestPets(unittest.TestCase):
         self.assertEqual(pets[0].category, "CAT")
 
     def test_passing_connection(self):
-        """ Pass in the Redis connection """
-        Pet.init_db(Redis(host='127.0.0.1', port=6379))
+        """Pass in the Redis connection"""
+        Pet.init_db(Redis(host=REDIS_HOST, port=REDIS_PORT))
         self.assertIsNotNone(Pet.redis)
 
     def test_passing_bad_connection(self):
-        """ Pass in a bad Redis connection """
-        self.assertRaises(ConnectionError, Pet.init_db, Redis(host='127.0.0.1', port=6300))
+        """Pass in a bad Redis connection"""
+        self.assertRaises(
+            ConnectionError, Pet.init_db, Redis(host="127.0.0.1", port=6300)
+        )
         self.assertIsNone(Pet.redis)
 
-    @patch.dict(os.environ, {'VCAP_SERVICES': json.dumps(VCAP_SERVICES)})
+    @patch.dict(os.environ, {"VCAP_SERVICES": json.dumps(VCAP_SERVICES)})
     def test_vcap_services(self):
-        """ Test if VCAP_SERVICES works """
+        """Test if VCAP_SERVICES works"""
         Pet.init_db()
         self.assertIsNotNone(Pet.redis)
 
-    @patch('redis.Redis.ping')
+    @patch("redis.Redis.ping")
     def test_redis_connection_error(self, ping_error_mock):
-        """ Test a Bad Redis connection """
+        """Test a Bad Redis connection"""
         ping_error_mock.side_effect = ConnectionError()
         self.assertRaises(ConnectionError, Pet.init_db)
         self.assertIsNone(Pet.redis)
@@ -229,7 +229,7 @@ class TestPets(unittest.TestCase):
 ######################################################################
 #   M A I N
 ######################################################################
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
     # suite = unittest.TestLoader().loadTestsFromTestCase(TestPets)
     # unittest.TextTestRunner(verbosity=2).run(suite)
